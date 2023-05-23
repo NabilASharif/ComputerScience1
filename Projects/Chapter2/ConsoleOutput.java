bd7dbb664e998433ba892145c94d0979b79840f2
Nabil Sharif <developerm01@gmail.com>
2023-05-23 18:17:35 -0400

    Create ConsoleOutput.java

---
+++ Projects/Chapter2/ConsoleOutput.java
@@ -0,0 +1,22 @@
+/*
+Nabil Sharif
+Introduction to Computer Science
+Chapter 2 - ConsoleOutput.java
+Description: This program demonstrates console output
+*/
+
+public class ConsoleOutput
+{
+	public static void main(String[] args)
+	{
+		//println statements advance to next line
+		System.out.println("One");
+		System.out.println("Two");
+		System.out.println("Three");
+		
+		//print statements DO NOT advance to next line
+		System.out.print("One");
+		System.out.print("Two");
+		System.out.print("Three");
+	}
+}
