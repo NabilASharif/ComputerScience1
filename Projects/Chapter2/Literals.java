3c96d7df567af90932da42bbaa2ef88ee08b16b8
Nabil Sharif <developerm01@gmail.com>
2023-05-24 02:26:28 -0400

    Create IntegerVariables and Literals

---
+++ Projects/Chapter2/Literals.java
@@ -0,0 +1,17 @@
+/*
+Nabil Sharif
+Introduction to Computer Science
+Chapter 2 - Literals.java
+Description: This program has literals and a variable
+*/
+
+public class Literals
+{
+	public static void main(String[] args)
+	{
+		int apples;
+		
+		apples = 20;
+		System.out.println("Today we sold " + apples + " bushels of apples.");
+	}
+}
