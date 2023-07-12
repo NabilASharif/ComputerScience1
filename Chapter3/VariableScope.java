6ff907db93f0e2e93f87577770b87fc23e4e0097
Nabil A. Sharif <developerm01@gmail.com>
2023-07-12 16:18:07 -0400

    Create VariableScope.java

---
+++ Chapter3/VariableScope.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+/**
+ * Nabil Sharif
+ * Introduction to Computer Science
+ *
+ * Chapter 3 - VariableScope .java
+ *
+ * Description:
+ *
+ * This program demonstrates how variables may be
+ * declared in various locations throughout a program.
+ *
+ */
+
+public class VariableScope 
+{
+	public static void main(String[] args)
+	{
+		// Create a Scanner object for keyboard input.
+		Scanner keyboard = new Scanner(System.in);
+		
+		// Get the user's first name.
+		System.out.print("Enter your first name: ");
+		String firstName;
+		firstName = keyboard.nextLine();
+		
+		// Get the user's last name.
+		System.out.print("Enter your last name: ");
+		String lastName;
+		lastName = keyboard.nextLine();
+		
+		// Display a message.
+		System.out.println("Hello " + firstName +
+				" " + lastName);
+		
+	} // end main
+
+} // end class
